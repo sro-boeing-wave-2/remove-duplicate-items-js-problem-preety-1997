effb8c08e2d1fb03feea44142b3bf6e89f7d65eb
Preety Kumari <preetydakshana@gmail.com>
2018-07-10 14:37:27 +0530

    Removing Duplicate using set

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const set = new Set(items);
+  return Array.from(set);
+};
 
 module.exports = removeDuplicateItems;
